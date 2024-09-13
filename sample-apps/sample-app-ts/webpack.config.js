f7d5629a254513397eef0482908e9331bae08245
Socrates Hlapolosa <socrates.hlapolosa@ghdlogistics.co.za>
2024-09-13 17:28:06 +0200

    removed watch

--- sample-apps/sample-app-ts/webpack.config.js
+++ sample-apps/sample-app-ts/webpack.config.js
@@ -8,14 +8,11 @@ module.exports = function() {
     entry: "./src/sampleAppController.ts",
     mode: "development",
     devtool: "source-map",
-    watch: true,
+    watch: false,  // Watch is now disabled
     stats: "errors-only",
     performance: {
       maxEntrypointSize: 300000,
       maxAssetSize: 300000
-    },
-    watchOptions: {
-      ignored: /node_modules/
     },
     output: {
       library: libraryName,
@@ -51,24 +48,19 @@ module.exports = function() {
       {
         apply: (compiler) => {
           compiler.hooks.done.tapAsync("done", function(stats, callback) {
-            if(!stats.compilation.errors || stats.compilation.errors.length === 0) {
-              // Clear the console on successful emit
+            if (!stats.compilation.errors || stats.compilation.errors.length === 0) {
               console.log("\u001b[2J\u001b[0;0H");
               console.log(`Build: ${compiledCount} ${buildOptions.output.filename} Completed.`);
               compiledCount += 1;
             }
-
             callback();
           });
         }
       }
     ]
   };
-  console.log("Creating FaceTec Sample:" + libraryName + " development build ....");
 
-  if(process.argv.indexOf("nowatch") > -1) {
-    buildOptions.watch = false;
-  }
+  console.log("Creating FaceTec Sample:" + libraryName + " development build ....");
 
   return buildOptions;
 };
